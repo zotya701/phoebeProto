cdac991f11006426b864929449dad0d9ac211d5e
zotya701 <zotya701@hotmail.hu>
2015-04-14 23:35:05 +0200

    Metódusok

--- Trap.java
+++ Trap.java
@@ -1,5 +1,53 @@
 package phoebeProto;
 
-public interface Trap {
+import java.awt.Point;
+import java.util.List;
 
+/**
+ *
+ */
+public interface Trap {
+	
+	/**
+	 * 
+	 * @param j
+	 */
+	public void interact(Jumping j);
+	
+	/**
+	 * 
+	 * @param nf
+	 */
+	public void setNormalField(NormalField nf);
+	
+	/**
+	 * 
+	 */
+	public void cleanup();
+	
+	/**
+	 * 
+	 * @param l
+	 * @return
+	 */
+	public boolean compareType(Landable l);
+	
+	/**
+	 * 
+	 * @param p
+	 */
+	public void setPosition(Point p);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Point getPosition();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public List<Trap> trapList();
+	
 }
