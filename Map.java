2b9c6aaab9061b604ea311be5d09ef74a4efe242
zotya701 <zotya701@hotmail.hu>
2015-04-15 14:39:08 +0200

    Map váz

--- Map.java
+++ Map.java
@@ -1,5 +1,96 @@
 package phoebeProto;
 
-public class Map {
+import java.awt.Point;
+import java.util.List;
 
+/**
+ * 
+ */
+public class Map implements Printable{
+	
+//Privát adattagok kezdete
+	/**
+	 * 
+	 */
+	private List<List<Field>> fields;
+
+	/**
+	 * 
+	 */
+	private List<List<Node>> nodes;
+
+	/**
+	 * 
+	 */
+	private Point size;
+
+	/**
+	 * 
+	 */
+	private List<Trap> trapList;
+	
+	/**
+	 * 
+	 */
+	private OutsideField outside;
+//Privát adattagok vége
+
+//publikus metódusok kezdete
+	/**
+	 * 
+	 * @param filename
+	 * @param trapList
+	 */
+	public Map(String filename, List<Trap> trapList){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void Print(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param coord
+	 * @return
+	 */
+	public Field getField(Point coord){
+		return fields.get(coord.y).get(coord.x);
+	}
+	
+	
+	public Point getNewPos(Point currentPos, Point vel){
+		currentPos.translate(vel.x, vel.y);
+		return currentPos;
+	}
+	
+	/**
+	 * 
+	 * @param s
+	 * @param d
+	 * @return
+	 */
+	public float calculateDistance(Point s, Point d){
+		return (float)(Math.pow((d.x-s.x)*(d.x-s.x)+(d.y-s.y)*(d.y-s.y), 0.5));
+	}
+	
+	/**
+	 * 
+	 * @param source
+	 * @return
+	 */
+	public Point getRouteToTrap(Point source){
+		return source;
+	}
+	
+	/**
+	 * 
+	 */
+	public void createGraph(){
+		
+	}
+//publikus metódusok vége
 }
