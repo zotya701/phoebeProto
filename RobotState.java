022bda98654d54aac8fae2e27f2f1c7a9d28ee3e
Szuchovszky <szuchovszky@mailbox.hu>
2015-04-17 22:49:59 +0200

    RobotState komment

--- RobotState.java
+++ RobotState.java
@@ -1,22 +1,22 @@
 package phoebeProto;
 
 /**
- * 
+ * A robot lehetségeit definiáló enum.
  */
 public enum RobotState {
 	
 	/**
-	 * 
+	 * Normál állapot.
 	 */
 	Normal,
 	
 	/**
-	 * 
+	 * A robot nem tudja módosítani a sebességvektorát ebben az állapotban.
 	 */
 	Unturnable,
 	
 	/**
-	 * 
+	 * Kiesett robot állapota.
 	 */
 	Eliminated
 	
